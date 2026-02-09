786e8bb68b04e232eaf052b3bda4582ca91548a0
kekeiirraa <keira08qh@gmail.com>
2026-02-09 16:42:05 +0800

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 const app = require('./app');
 // port number the server will listen on (default 3000)
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // start server and listen on port; log when ready to accept connections
 app.listen(port, () => {
